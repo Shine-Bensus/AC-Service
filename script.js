f970a1d793f0a90f64dd682881d7b6a5733e2f75
Shine-Bensus <167521183+Shine-Bensus@users.noreply.github.com>
2024-05-15 11:07:52 +0530

    1
    1

---
+++ script.js
@@ -0,0 +1,15 @@
+function home(){
+    var home1=document.getElementById("home");
+    home1.style.scrollMarginTop="70px";
+}
+function service(){
+    var service1=document.getElementById("service");
+    service1.style.scrollMarginTop="70px";
+}
+function contact(){
+    var contact1=document.getElementById("contact");
+    contact1.style.scrollMarginTop="70px";
+}
+
+
+
